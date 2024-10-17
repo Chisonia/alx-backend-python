3a656dedfdddfb5ed06712c12443c047c5ad0c4d
chisonia <sonya12slyvia@gmail.com>
2024-10-17 10:55:48 +0100

    Concatenate

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,4 +4,5 @@ and sums then to give a float result'''
 
 
 def add(a: float, b: float) -> float:
+    '''This functions accepts 2 float arguments and sums them'''
     return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''This module contains a function that concatenates 2 strings'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''This function takes 2 string arguments and concatenates them '''
+    return (f"{str1}{str2}")
