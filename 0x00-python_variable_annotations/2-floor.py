8795d5f76f72859982c1c74db7a42b69ff15471e
chisonia <sonya12slyvia@gmail.com>
2024-10-17 11:05:16 +0100

    Floor of float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+'''The module is a function that return floor of float'''
+
+
+def floor(n: float) -> int:
+    '''This function takes a float n and returns its floor as an integer'''
+    return math.floor(n)
