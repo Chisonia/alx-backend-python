d14c14d6c8894b34915c1e7457c5887ecde18b1b
chisonia <sonya12slyvia@gmail.com>
2024-10-18 14:40:28 +0100

    sorted

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -14,4 +14,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     list of delays in ascending order."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
-    return delays
+    return sorted(delays)
